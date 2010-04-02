30baf9a90b58ceb11266139bcb4dec7ac0b97a4e
Jeff Balogh <me@jeffbalogh.org>
2010-04-03 05:56:25 +0800

    pass transform_fns down through clones

--- queryset_transform/__init__.py
+++ queryset_transform/__init__.py
@@ -4,11 +4,16 @@ class TransformQuerySet(models.query.QuerySet):
     def __init__(self, *args, **kwargs):
         super(TransformQuerySet, self).__init__(*args, **kwargs)
         self._transform_fns = []
-    
+
+    def _clone(self, klass=None, setup=False, **kw):
+        c = super(TransformQuerySet, self)._clone(klass, setup, **kw)
+        c._transform_fns = self._transform_fns
+        return c
+
     def transform(self, fn):
         self._transform_fns.append(fn)
         return self
-    
+
     def iterator(self):
         result_iter = super(TransformQuerySet, self).iterator()
         if self._transform_fns:
@@ -19,6 +24,6 @@ class TransformQuerySet(models.query.QuerySet):
         return result_iter
 
 class TransformManager(models.Manager):
-    
+
     def get_query_set(self):
         return TransformQuerySet(self.model)
